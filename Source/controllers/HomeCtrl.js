bfa1125c412ec5740e8f5d361c3548076462bb88
Vinicius Zorzanelli <vinizorza@hotmail.com>
2017-10-24 11:08:34 -0200

    att homectrl

--- Source/controllers/HomeCtrl.js
+++ Source/controllers/HomeCtrl.js
@@ -1,13 +1,21 @@
 app.controller("HomeCtrl",function($scope){
-    $scope.opcoes = [
-        {nome: "Cardápio", src:"Cardapio.html"},
+	
+	
+	
+	var host = location.protocol +"//"+ location.host;
+	
+	
+	
+    $scope.opcoes = [		    
+        {nome: "Cardápio", src: host + "Cardapio.html"},
         {nome: "Relatório", src:"Relatorio.html"},
         {nome: "Estoque", src:"Estoque.html"},
         {nome: "Cadastrar Bebida", src:"CadastraBebida.html"},
-        {nome: "Pedidos", src:"/fastfrango_frontend/Source/#!/novopedido"},
-        {nome: "Novo Pedido", src:"./fastfrango_frontend/Source/#!/novopedido"},
+        {nome: "Pedidos", src:"/novopedido"},
+        {nome: "Novo Pedido", src: host + "/#!/novopedido"},
         {nome: "Cadastrar Combo", src:"CadastraCombo.html"},
         {nome: "Cadastrar Porção", src:"CadastraPorcao.html"},
         {nome: "Alterar Produto", src:"AlteraProdutos.html"},
     ];
+    
 });
