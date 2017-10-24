5be72f21e0928d2865bf6df04cb13171e73f963d
Vinicius Zorzanelli <vinizorza@hotmail.com>
2017-10-23 23:37:54 -0200

    Atualizado rotas

--- Source/controllers/HomeCtrl.js
+++ Source/controllers/HomeCtrl.js
@@ -1,3 +1,13 @@
 app.controller("HomeCtrl",function($scope){
-    $scope.mensagem = "Bem vindo";
+    $scope.opcoes = [
+        {nome: "Cardápio", src:"Cardapio.html"},
+        {nome: "Relatório", src:"Relatorio.html"},
+        {nome: "Estoque", src:"Estoque.html"},
+        {nome: "Cadastrar Bebida", src:"CadastraBebida.html"},
+        {nome: "Pedidos", src:"/fastfrango_frontend/Source/#!/novopedido"},
+        {nome: "Novo Pedido", src:"./fastfrango_frontend/Source/#!/novopedido"},
+        {nome: "Cadastrar Combo", src:"CadastraCombo.html"},
+        {nome: "Cadastrar Porção", src:"CadastraPorcao.html"},
+        {nome: "Alterar Produto", src:"AlteraProdutos.html"},
+    ];
 });
