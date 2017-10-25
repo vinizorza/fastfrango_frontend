90bfe53a3b07267ecb202c9815d4e4b696c6c512
Vinicius Zorzanelli <vinizorza@hotmail.com>
2017-10-24 23:03:51 -0200

    att

--- Source/controllers/NovoPedidoCtrl.js
+++ Source/controllers/NovoPedidoCtrl.js
@@ -2,7 +2,15 @@ app.controller("NovoPedidoCtrl",function($scope){
 
     $scope.combos = [];
     $scope.porcoes = [];
+    $scope.observacao = "";
+
     $scope.combosCadastrados = [
+        {id:"1", nome:"Kit Doidi"},
+        {id:"2", nome:"Vem Gordura"},
+        {id:"3", nome:"Ventania"},
+    ];
+
+    $scope.porcoesCadastradas = [
         {id:"1", nome:"Batata Suprema"},
         {id:"2", nome:"Aipim Doido"},
         {id:"3", nome:"Polenta Maluca"},
@@ -13,8 +21,17 @@ app.controller("NovoPedidoCtrl",function($scope){
         delete $scope.combo;
     };
 
+    $scope.adicionarPorcao = function(porcao){
+        $scope.porcoes.push(angular.copy(porcao));
+        delete $scope.porcao;
+    };
+
     $scope.excluirCombo = function(index){
         $scope.combos.splice(index, 1);
+    };
+
+    $scope.excluirPorcao = function(index){
+        $scope.porcoes.splice(index, 1);
     };
     
     
