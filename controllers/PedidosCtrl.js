782e17b5dbe2789479554b821b6e396de2f3dac5
Vinicius Zorzanelli <vinicius.zorzanelli@alta-rt.com>
2017-10-27 17:20:57 -0200

    Atualizado paginas pedidos

--- controllers/PedidosCtrl.js
+++ controllers/PedidosCtrl.js
@@ -1,3 +1,10 @@
 app.controller("PedidosCtrl",function($scope){
+
+    var screenHeight = $(window).height()-110 + "px";
+
+    $scope.colunaStyle = {
+        "height" : screenHeight,
+        "overflow" : "auto"
+    };
     
 });
