e8c07bb30383e2062fa0253494cc016d018df29e
Vinicius Zorzanelli <vinizorza@hotmail.com>
2017-10-30 22:44:05 -0200

    terminado tela de pedidos

--- controllers/PedidosCtrl.js
+++ controllers/PedidosCtrl.js
@@ -3,8 +3,19 @@ app.controller("PedidosCtrl",function($scope){
     //Na hora de pescorrer o array, filtrar pelo status
     //Ex: <li ng-repeat="pedido in todosPedidos | filter: pendente">{{name}}</li>
     $scope.todosPedidos = [
-        {id: 1, nomeCliente:"Vinicius", combos:{id: 1, nome: "Ventania"} , status: "pendente"}
+        {id: 1, nomeCliente:"Vinicius", local:"Rua H, 2015, Castelândia, Serra", combos: "Ventania", porcoes:"Batata", bebidas:"Coca Cola 2L", preco: 35.5, status: "pendente"},
+        {id: 2, nomeCliente:"Mesa 08", local:"local", combos: "Hot Quente", porcoes:"Aipim", bebidas:"Suco Laranja 300ml", preco: 38.5, status: "pendente"},
+        {id: 3, nomeCliente:"Letícia", local:"Rua R, 1080, Castelândia, Serra", combos: "Vesgueira", porcoes:"Batata", bebidas:"Coca Cola 2L", preco: 42.5, status: "fazendo"},
+        {id: 4, nomeCliente:"Mesa 05", local:"local", combos: "Mariposa", porcoes:"Polenta", bebidas:"Coca Cola 2L", preco: 57.5, status: "fazendo"}        
     ];
+
+    $scope.finalizarPedido = function(pedido){
+        pedido.status = "finalizado";
+    }
+
+    $scope.confirmarPedido = function(pedido){
+        pedido.status = "fazendo";
+    }
 
 
 
